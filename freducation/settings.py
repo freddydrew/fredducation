e5f91b46a905c3d54ac59fb4c76702b5eaac7907
freddy <freddydrew@gmail.com>
2023-08-22 08:38:40 -0400

    allowed hosts edit

--- freducation/settings.py
+++ freducation/settings.py
@@ -28,9 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == "1"
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
-if not DEBUG:
-    ALLOWED_HOSTS += [os.environ.get('DJANGO_ALLOWED_HOSTS')]
+ALLOWED_HOSTS = [os.environ.get('DJANGO_ALLOWED_HOSTS')]
 
 
 # Application definition
