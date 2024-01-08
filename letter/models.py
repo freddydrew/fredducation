82a0e5b9e987c1b902a660702e83f61ee4762a83
freddy <freddydrew@gmail.com>
2024-01-08 09:10:40 -0500

    No foreign key in letter, all rights change
    I changed the all rights reserved year to 2024, I also got rid
    of the foreign key in the letter model as I dont need it.

--- letter/models.py
+++ letter/models.py
@@ -1,13 +1,10 @@
 from django.db import models
-from articles.models import article
 import datetime as dt
 
 # Create your models here.
 class letter(models.Model):
     title = models.CharField(max_length=75,blank=False)
     message = models.TextField()
-    article = models.ForeignKey(article,on_delete=models.PROTECT,
-                                blank=True)
     sent = models.BooleanField(default=False,editable=False)
     dateSent = models.DateField(null=True,editable=False)
 
