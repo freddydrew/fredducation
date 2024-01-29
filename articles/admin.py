115f071ad293f7c8565eaf4b689fd82aaf370a2d
freddy <freddydrew@gmail.com>
2024-01-29 18:00:30 -0500

    Comment admin, utils files under articles/

--- articles/admin.py
+++ articles/admin.py
@@ -1,17 +1,32 @@
 from django.contrib import admin
 from .models import article, articleImage
 
-# All my models registered here so we can see them in the admin
-class articleImageInline(admin.StackedInline):
-    model = articleImage
-    extra = 0
+# Here I Register models so they can be seen in the admin panel.
+# I also specify the types of models and it's relation to others.
 
+'''
+This inline model will be shown below an Article model if I choose
+to add one. First I must tell Django, 'this will be an inline'
+'''
+class articleImageInline(admin.StackedInline):
+    model = articleImage #specify the model to be an inline
+    extra = 0 #default is 0 to be shown under another model
+
+# Main model for articles, its the blueprint for posts on the site
 class articleAdmin(admin.ModelAdmin):
     # What fields I will see in the multiple objects view
     list_display = ['title','publish','publishDate','postType','country']
-    # For the uneditable fields
+    # For the uneditable fields to be seen as well
     readonly_fields = ['timestamp','updated','alpha3','slug']
+
+    '''
+    So I can add or subtract Image Model Objects from a given Article 
+    Model Object, in the same window. This allows me to easily see and
+    change what Images are attached to each Article. I did not 
+    register this model seperately because I only wish to edit it 
+    from the Articles that it's objects will be attached to. 
+    '''
     inlines = [articleImageInline]
 
-# Register the model
+# Register the model in this Django App, the article and its admin
 admin.site.register(article,articleAdmin)
--- articles/utils.py
+++ articles/utils.py
@@ -1,11 +1,17 @@
 import re #for regexp
 from unidecode import unidecode #transliterate spanish 
 
+'''
+This function will take the provided string and make it ready to 
+be used as a slug. A slug will be used in the unique URL for a given 
+post on this site and cannot contain non-Ascii characters or spaces. 
+'''
+
 def slugfiy(str):
     #to lower case, lose trailing/leading whitespace
     str = str.lower().strip() 
     str = unidecode(str) #replace spanish chars with ascii
-    #go through regexp patterns and replaces with spaces and dash
+    #checks regexp patterns and replaces chars with spaces and dash
     str = re.sub(r'[^\w\s-]', '', str)
     str = re.sub(r'[\s_-]+', '-', str)
     str = re.sub(r'^-+|-+$', '', str)
